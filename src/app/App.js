88532c67f5b7fecd519ba6ffa2304e0757c7bc86
MAbdurahman <mdbdrrhm@gmail.com>
2021-09-14 08:32:44 -0500

    Add Question.jsx and modify App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import data from './../data';
+import SingleQuestion from './../components/Question';
 
 function App() {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+	const [questions, setQuestions] = useState(data);
+	return (
+		<main>
+			<div className='container'>
+				<h3>questions and answers about login</h3>
+				<section className='info'>
+					{questions.map(question => {
+						return (
+							<SingleQuestion
+								key={question.id}
+								{...question}
+							></SingleQuestion>
+						);
+					})}
+				</section>
+			</div>
+		</main>
+	);
 }
 
 export default App;
